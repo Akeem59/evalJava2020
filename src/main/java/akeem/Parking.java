8687de52cb27ad8d31c606a8817089615d4baea2
@k€€m <akeem.baha@gmail.com>
2020-02-12 15:26:30 +0100

    liste de vehicules autorises à stationner

---
+++ src/main/java/akeem/Parking.java
@@ -0,0 +1,18 @@
+package akeem;
+
+import java.util.ArrayList;
+import java.util.List;
+ 
+class Parking{
+
+    public void vehiculeAutorises() {
+        // liste des véhicules autorisés
+        
+        List<String> list = new ArrayList<String>();
+      list.add("XX-777-ZZ");
+      list.add("DD-222-VV");
+      list.add("AA-444-ZC");
+  
+    }
+
+}
